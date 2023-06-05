d2b87e2e50ac7cac38fcd4cf767d42cd5ba0d0fc
yujinomoto <codoo5555@gmail.com>
2023-06-05 10:32:35 +0800

    Hello

--- ongots.c
+++ ongots.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+// sok test
 // Board-nii hemjee 10x10
 // Massive 0-ees eheldeg ba board-iig 1-ees ehluulhiin tuld SIZE=11
 #define SIZE 11
@@ -70,9 +72,6 @@ void duusgah()
 int main()
 {
     // sok test
-
-
-
 
 
 
